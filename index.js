c0405824fe85184687de3ae257e3d68934e3d960
abubakkarsiddik123 <abu089604@gmail.com>
2026-09-19 17:18:44 +0600

    add ideas API with MongoDB integration

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-const express = require('express');
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 8080;
+const uri = process.env.MONGODB_URI;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("ideavaultDB");
+    const ideasCollection = db.collection("ideas");
+
+    app.get("/ideas", async (req, res) => {
+      const result = await ideasCollection.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
